fcf9b3c428b18b8c654eddf180fefbd1c3c49e04
Leonard develops <120578632+leonard-roepcke@users.noreply.github.com>
2025-09-17 08:29:00 +0000

    3 layer

---
+++ Controller.java
@@ -0,0 +1,11 @@
+
+public class Controller{
+    private Ui ui;
+    private Logic logic;
+
+    public Controller() {
+        System.out.println("Controller initialized");
+        ui = new Ui(this);
+        logic = new Logic();
+    };
+}
