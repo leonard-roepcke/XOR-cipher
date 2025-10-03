9b11ccb3b1a5993693c43e6f0089e4e9480d9d4d
Leonard develops <120578632+leonard-roepcke@users.noreply.github.com>
2025-10-03 16:32:22 +0000

    test

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("XOR Cipher Program");
+        System.out.println("XOR Cipher Program !");
 
         Controller controller = new Controller();
     }
