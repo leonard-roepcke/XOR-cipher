d17c8d4d0bf381de9315886920bb3708d14cb1a5
Jakob Müller <109761119+TxJakob@users.noreply.github.com>
2025-10-07 06:45:20 +0000

    konflikt-test

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("XOR Cipher Program! Leonards Programm-Fork");
         System.out.println("XOR Cipher Program! Leonards Programm-Forkv 3");
+        System.out.println("Konflikt Test");
 
         Controller controller = new Controller();
     }
