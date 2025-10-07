d50626925435d315e1cc68368b277dd4baad5494
Jakob Müller <109761119+TxJakob@users.noreply.github.com>
2025-10-07 06:37:32 +0000

    jakob-test2

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("XOR Cipher Program! Leonards Programm-Fork");
-        System.out.println("XOR Cipher Program! Leonards Programm-Fork 2");
+        System.out.println("XOR Cipher Program! Leonards Programm-Forkv 3");
 
         Controller controller = new Controller();
     }
